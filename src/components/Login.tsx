e7426ab0729bfda0eddc0f7afecda6d127b56264
Nstellatos <n_stellatos@yahoo.com>
2022-07-07 19:37:01 -0400

    add login add validation / style tweaks

---
+++ src/components/Login.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Center,
+  Box,
+  Button,
+  Text,
+} from "@chakra-ui/react";
+import { nanoid } from "nanoid";
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  /*
+    Using the react-hook-form package here because it's faster and if I used the useState react hook along
+    with updating state on every keypress to create a controlled form. State would cause
+    a re-render on every keypress. Probably doesn't matter with only 2 inputs but on a larger scale
+    it could cause some slowdowns.
+  */
+  const renderErrorMessages = () => {
+    //use nanoid package to generate uuid for key so each FormErrorMessage rendered to the dom has a unique key
+    return Object.values(errors).map((error) => (
+      <FormErrorMessage key={nanoid()}>
+        <Text fontSize="xs">{error.message}</Text>
+      </FormErrorMessage>
+    ));
+  };
+  return (
+    <Center h="100vh">
+      <Box w="sm">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl isInvalid={errors.username || errors.password}>
+            <FormLabel htmlFor="username">Username</FormLabel>
+            <Input
+              id="username"
+              type="text"
+              {...register("username", {
+                required: "Please enter a valid username",
+              })}
+            />
+
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password_textbox"
+              type="password"
+              {...register("password", {
+                required: "Please enter a valid password",
+              })}
+            />
+            <Button mt={5} colorScheme="green" type="submit">
+              Login
+            </Button>
+            <Center>
+              <div
+                className="error_container"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginTop: "1rem",
+                }}
+              >
+                {errors ? renderErrorMessages() : ""}
+              </div>
+            </Center>
+          </FormControl>
+        </form>
+      </Box>
+    </Center>
+  );
+};
+
+export default Login;
